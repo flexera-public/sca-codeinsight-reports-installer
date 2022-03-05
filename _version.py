097fa6d0c8ac83e36ae240f5de20f885b1dc95af
Sean Geary <sgeary@flexera.com>
2022-03-05 10:45:10 -0500

    Bump to v1.0.4

---
+++ _version.py
@@ -0,0 +1,10 @@
+'''
+Copyright 2022 Flexera Software LLC
+See LICENSE.TXT for full license text
+SPDX-License-Identifier: MIT
+
+Author : sgeary  
+Created On : Sat Mar 05 2022
+File : _version.py
+'''
+__version__ = "1.0.4"
